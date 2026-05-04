34a43b7067e00a84de834bdb375673929258c5bd
Vida96 <nikolavidovic47@gmail.com>
2026-05-04 12:48:56 +0200

    Update vulnerable_script.py

--- 4-sast/vulnerable_script.py
+++ 4-sast/vulnerable_script.py
@@ -1,10 +1,13 @@
 # this file contains a SQL injection vulnerability
 
 def get_user_data(username):
-    # Vulnerable to SQL injection due to unsanitized input
-    query = "SELECT * FROM users WHERE username = '" + username + "'"
-    # Simulated execution of the query
-    print("Executing query:", query)
+    query = "SELECT * FROM users WHERE username = %s"
+    params = (username,)
+    
+    cursor.execute(query, params)
+    
+    # Simulated execution output (safe logging)
+    print("Executing query with params:", query, params)
 
 
 if __name__ == "__main__":
